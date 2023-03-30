ff6acfc83dd30f833afb089e842c325ca3cbf3a1
Pavel <savin.pavel.a@gmail.com>
2023-03-30 18:58:55 +0400

    Added menu with authorization & registration into ClientServiceImpl

--- src/main/java/ru/itsjava/services/ClientServiceImpl.java
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -20,6 +20,7 @@ public class ClientServiceImpl implements ClientService {
             // Создаём новый поток, пихаем в него наш класс для чтения потока (String) с сервера.
             new Thread(new SocketRunnable(socket)).start();
 
+
             // Для того чтобы отправить текстовое сообщение на сервер, необходимо создать исходящий поток.
             // Чтобы создать поток, используем метод getOutputStream() на нашем созданном socket
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
@@ -28,27 +29,73 @@ public class ClientServiceImpl implements ClientService {
             // Используем наш написанный MessageInputService с BufferedReader внутри для этой задачи:
             MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
 
-            // Сначала авторизируем пользователя:
-            System.out.print("Введите логин: ");
-            String login = messageInputService.getMessage();
-            System.out.print("Введите пароль: ");
-            String password = messageInputService.getMessage();
-            // Отправляем данные на сервер:
-            // !autho!login:password
-            serverWriter.println("!autho!" + login + ":" + password);
-            serverWriter.flush();
-
-            while (true) {
-                String consoleMessage = messageInputService.getMessage();
+            // Меню
+            while(true){
+                System.out.println("**** CHAT ****");
+                System.out.println(" 1 -- Войти в чат");
+                System.out.println(" 2 -- Новый пользователь");
+                System.out.println(" exit -- Выйти из чата");
+                String menuNum = messageInputService.getMessage();
+                // Авторизиризация пользователя:
+                if (menuNum.equals("1")){
+                    System.out.println("**** Авторизация ****");
+                    System.out.print("Введите логин: ");
+                    String login = messageInputService.getMessage();
+                    System.out.print("Введите пароль: ");
+                    String password = messageInputService.getMessage();
+                    // Отправляем данные на сервер:
+                    // !autho!login:password
+                    serverWriter.println("!autho!" + login + ":" + password);
+                    serverWriter.flush();
+                    System.out.println("**** Авторизация прошла успешно ****");
+                    System.out.println("**** Напишите сообщение ****");
+                    // Цикл для отправки сообщения пользователем:
+                    while (true) {
+                        String consoleMessage = messageInputService.getMessage();
+                        // Выход из чата:
+                        if (consoleMessage.equals("exit")) {
+                            serverWriter.println(consoleMessage);
+                            serverWriter.flush();
+                            System.out.println("Выход из цикла...");
+                            break;
+                        }
+                        // Пишем текст:
+                        serverWriter.println(consoleMessage);
+                        // Чтобы данные не буфферизовались, отправляем данные на сервер методом flush()
+                        serverWriter.flush();
+                    }
+                }
+                // Регистрация пользователя:
+                if (menuNum.equals("2")){
+                    System.out.println("**** Регистрация нового пользователя ****");
+                    System.out.print("Введите логин: ");
+                    String login = messageInputService.getMessage();
+                    System.out.print("Введите пароль: ");
+                    String password = messageInputService.getMessage();
+                    // Отправляем данные на сервер:
+                    // !reg!login:password
+                    serverWriter.println("!reg!" + login + ":" + password);
+                    serverWriter.flush();
+                    System.out.println("**** Регистрация прошла успешно ****");
+                    System.out.println("**** Напишите сообщение ****");
+                    // Цикл для отправки сообщения пользователем:
+                    while (true) {
+                        String consoleMessage = messageInputService.getMessage();
+                        // Выход из чата:
+                        if (consoleMessage.equals("exit")) {
+                            System.out.println("Выход из цикла...");
+                            break;
+                        }
+                        // Пишем текст:
+                        serverWriter.println(consoleMessage);
+                        serverWriter.flush();
+                    }
+                }
                 // Выход из чата:
-                if (consoleMessage.equals("exit")) {
+                if (menuNum.equals("exit")) {
                     System.out.println("Выход из цикла...");
                     break;
                 }
-                // Пишем текст:
-                serverWriter.println(consoleMessage);
-                // Чтобы данные не буфферизовались, отправляем данные на сервер методом flush()
-                serverWriter.flush();
             }
             System.out.println("Закрываем соединение...");
             socket.close();
